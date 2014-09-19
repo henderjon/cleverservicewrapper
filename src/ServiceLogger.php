294f3ede8704755a6dbb307878d59b41c3b32485
henderjon <jhenderson@capstonedigital.com>
2014-09-19 14:57:13 -0500

    adds stock logger

---
+++ src/ServiceLogger.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Clever;
+
+use \Psr\Log;
+
+class ServiceLogger extends Log\AbstractLogger {
+
+	function log($level, $message, array $context = []){
+
+		$context = ["level" => strtoupper($level), "message" => $message, "timestamp" => date("c (e)")] + $context;
+
+		$len = 0;
+		foreach($context as $key => $value){
+			if( ($l = strlen($key)) > $len){ $len = $l; }
+		}
+
+		$output = "\n\n" . str_repeat("-", 72) . "\n\n";
+
+		foreach($context as $key => $value){
+			$output .= sprintf("%{$len}s => (%s)%s\n", $key, gettype($value), json_encode($value));
+		}
+
+		$output .= "\n\n\n";
+
+		echo $output;
+
+	}
+
+}
