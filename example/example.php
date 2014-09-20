a521c49b2fbd34a6a1582abae6e4ae51f47ad236
henderjon <jhenderson@capstonedigital.com>
2014-09-19 22:02:19 -0500

    makes prettier example

--- example/example.php
+++ example/example.php
@@ -3,13 +3,13 @@
 require "vendor/autoload.php";
 
 $clever   = new \Clever\ServiceWrapper("DEMO_TOKEN", new \Clever\ServiceLogger);
-$district = $clever->getCleverDistrict("4fd43cc56d11340000000005a"); // mess wtih this value to see the output
+$district = $clever->getCleverDistrict("4fd43cc56d11340000000005"); // mess wtih this value to see the output
 
 if(!$district){
 	exit(1);
 }
 
-$params = ["limit" => 2];
+$params["limit"] = 2;
 while( $events = $clever->ping( $district, "events", $params ) ) {
 	foreach($events as $event){
 		$params["starting_after"] = $event->id;
