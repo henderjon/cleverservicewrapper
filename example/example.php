8e6b336a4fcd4c27819101530f8b04ac3b01b131
henderjon <jhenderson@capstonedigital.com>
2015-09-02 12:51:15 -0500

    drop drop

--- example/example.php
+++ example/example.php
@@ -5,11 +5,11 @@ namespace Clever;
 require "vendor/autoload.php";
 
 $request = new Request;
-$request->setAuthorization("DEMO_TOKENZ");
+$request->setAuthorization("DEMO_TOKEN");
 
 $service = new Service($request, new ServiceLogger);
 
 $D = new CleverDistrict($service, "4fd43cc56d11340000000005");
 
-drop($D->schools());
+drop(json_decode($D->schools())->data[0]->data);
 
